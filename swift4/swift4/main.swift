09df76e59f31bc071c16bef3d4f1cd8f85fa1ab4
Luo-Yao <johnny.xiao@hotmail.com>
2017-10-25 14:44:42 +0800

    init swift-4

---
+++ swift4/swift4/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  swift4
+//
+//  Created by Luo Yao on 2017/10/25.
+//  Copyright © 2017年 Luo Yao. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
---
+++ test/test/Test.swift
@@ -0,0 +1,13 @@
+//
+//  Test.swift
+//  test
+//
+//  Created by Luo Yao on 2017/10/25.
+//  Copyright © 2017年 johnny. All rights reserved.
+//
+
+import UIKit
+
+class Test: NSObject {
+
+}
