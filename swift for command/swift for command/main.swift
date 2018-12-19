e9c5c6a0e984c5f73aa2c9a0f9abfd381f416370
Luo-Yao <luoyao@ti-net.com.cn>
2018-12-19 10:54:37 +0800

    update

---
+++ swift for command/swift for command/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  swift for command
+//
+//  Created by luoyao on 2018/12/19.
+//  Copyright © 2018 luoyao. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
